64e54a0fe09871936fdf98a7a4913296ad89c89a
Das6013 <andru-lazancev@mail.ru>
2022-02-23 21:31:04 +0700

    comment

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     stack<int> kek;
     cout<<kek.Size()<<endl;
-    kek.Push(1);
-    kek.Push(2);
-    kek.Iter();
-    kek.Pop();
+    kek.Push(1);//add 1
+    kek.Push(2);// add 2
+    kek.Iter();// show stack
+    kek.Pop();// Вытащил последний элемент
     cout<<endl;
-    kek.Iter();
+    kek.Iter();//show stack
     stack<char> lol;
-    lol.Pop();
+    lol.Pop();//check except
 
 
     return a.exec();
