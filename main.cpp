9c26009be6b4d5254dddc4591568aa239dc1af82
Das6013 <andru-lazancev@mail.ru>
2022-02-23 20:35:23 +0700

    Расписал класс стек и его методы,проверил работу метода Size()

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <QCoreApplication>
-
+#include <stack.h>
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    stack<int> kek;
+    cout<<kek.Size();
     return a.exec();
 }
--- stack.h
+++ stack.h
@@ -1,4 +1,60 @@
 #ifndef STACK_H
 #define STACK_H
+#include<iostream>
+#include <functional>
+using namespace std;
+
+template <class T>
+
+class stack
+{
+    struct Node//список
+    {
+        const T value;//Значения переменной
+        Node *prev=nullptr;// указатель на прошлое звено
+
+    };
+    Node *back=nullptr;//последний элемент
+    int size=0;//размер
+public:
+    ~stack();//Деструктор
+    void Push(const T &value);//Добавления элемента
+    const T Pop();//извлечения значения из стека
+    void Iter(function<void(const T &value)>f) const;//Проход по стеку
+    int Size();//Размер
+    void Clear();//очистка
+
+};
+template <class T>
+void stack<T>::Push(const T &value)
+{
+   Node *node=new Node(value,back);
+   if (node==nullptr)
+   {
+       cout<<"kek"<<endl;
+   }
+   back=node;
+   size++;
+}
+template <class T>
+void stack<T>::Clear()
+{
+    while (back!=nullptr)
+    {
+        Node *last=back;//Последний элемент
+        back=back->prev;
+        delete last;
+    }
+}
+template <class T>
+stack<T>::~stack()//Деструктор
+{
+    Clear();
+}
+template <class T>
+int stack<T>::Size()
+{
+    return size;
+}
 
 #endif // STACK_H
