ab24e1caca46cbaa735aa5bd3ecf14ba25d4a6b4
Das6013 <andru-lazancev@mail.ru>
2022-03-08 17:28:45 +0700

    write methods class person

--- person.h
+++ person.h
@@ -1,11 +1,21 @@
 #ifndef PERSON_H
 #define PERSON_H
-
+#include<iostream>
+using namespace std;
 
 class Person
 {
+private:
+    long key;
+    string last_name;
+    string first_name;
 public:
-    Person();
+        Person();
+        Person(const Person&);
+        const string& getLastName() const { return last_name; }
+        const string& getFirstName() const { return first_name; }
+        void setLastName(const std::string&);
+        void setFirstame(const std::string&);
 };
 
 #endif // PERSON_H
