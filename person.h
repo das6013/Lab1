60031d57ed368adc22c93bfc0bd15bdebfbacad1
Das3013 <46927879+das6013@users.noreply.github.com>
2022-06-30 11:59:48 +0700

    add comment

--- person.h
+++ person.h
@@ -14,11 +14,11 @@ public:
 public:
     Person();//конструктор по умолчанию
     Person(QString FIO);//Конструтор одной строки
-    Person(const Person&);
-    void setSurname(QString _surname);
+    Person(const Person&);//consructor for person
+    void setSurname(QString _surname);//setters
     void setName(QString _name);
     void setPatronymic(QString _patronymic);
-    const QString& getSurname() const { return surname; }
+    const QString& getSurname() const { return surname; }//getters
     const QString& getName() const { return name; }
     const QString& getPatronymic() const { return patronymic; }
 
